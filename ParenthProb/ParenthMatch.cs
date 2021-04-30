1e0997abea84de5317d79c9638d72f93de2f019c
LinkaiShao <linkai.k.shao@gmail.com>
2021-04-29 17:30:27 -0700

    parenthathese check
    c#

---
+++ ParenthProb/ParenthMatch.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleInterviewParenthathese
+{
+    public class ParenthMatch
+    {
+        public int FindChar(string input, char find, int start)
+        {
+            for(int i = start; i < input.Length; i++)
+            {
+                if(input[i] == find)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool CheckParenthMatch(string input)
+        {
+            int start = 0;
+            int start1 = 0;
+            while(true)
+            {
+                int temp = FindChar(input, '(', start);
+                int temp1 = FindChar(input, ')', start1);
+                // can't find (
+                if(temp==-1)
+                {
+                    if(temp1 != -1)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else if(temp > temp1)
+                {
+                    return false;
+                }
+                start = temp + 1;
+                start1 = temp1 + 1;
+            }
+        }
+        public bool CheckParenthMatch(string input, char[] characters)
+        {
+            for(int i = 0; i< characters.Length;i++)
+            {
+                int start = 0;
+                int start1 = 0;
+                while (true)
+                {
+                    int temp = FindChar(input, characters[i], start);
+                    int temp1 = FindChar(input, characters[i], start1);
+                    // can't find (
+                    if (temp == -1)
+                    {
+                        if (temp1 != -1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else if (temp > temp1)
+                    {
+                        return false;
+                    }
+                    start = temp + 1;
+                    start1 = temp1 + 1;
+                }
+                
+            }
+            return true;
+
+        }
+    }
+}
---
+++ ParenthProb/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GoogleInterviewParenthathese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iput;
+            
+            iput = Console.ReadLine();
+            var res = new ParenthMatch();
+            if(res.CheckParenthMatch(iput))
+            {
+                Console.WriteLine("works");
+            }
+            else
+            {
+                Console.WriteLine("doesn't work");
+            }
+        }
+    }
+}
