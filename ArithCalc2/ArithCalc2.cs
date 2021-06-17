5517705dab62ef7981e428f08ee8f7a0f2431703
LinkaiShao <linkai.k.shao@gmail.com>
2021-06-17 15:47:07 -0700

    error handlings
    parenth matching, weird characters, allow space, bad arithmatic presentation such as 1++2 are handled

--- ArithCalc2/ArithCalc2.cs
+++ ArithCalc2/ArithCalc2.cs
@@ -1,12 +1,66 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ArithCalcV2
 {
 
-    static class ArithCalcv2
+    internal static class ArithCalcv2
     {
+        public static double Run(string input)
+        {
+            string nonspace;
+            ValidateInput(input, out nonspace);
+            return Calc(input);
+        }
+        private static bool ValidateInput(string input, out string nonSpace)
+        {
+            List<char>nonSpaceInput = new List<char>();
+            string cur = "";
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    cur = "leftP";
+                    nonSpaceInput.Add(input[i]);
+                }
+                else if (input[i] == ')')
+                {
+                    cur = "rightP";
+                    nonSpaceInput.Add(input[i]);
+                }
+                else if (char.IsNumber(input[i]))
+                {
+                    cur = "num";
+                    nonSpaceInput.Add(input[i]);
+                }
+                else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
+                {
+                    // prior to arith has to be either right ) or num
+                    if (cur != "rightP"||cur != "num") {
+                        Console.WriteLine("error occurs at position, need either number of ) before arithmatic {0}", i);
+                        nonSpace = "";
+                        return false;
+                    }
+                    char temp = input[i + 1];
+                    if (temp!= '(' || !char.IsNumber(temp)){
+                        nonSpace = "";
+                        Console.WriteLine("error occurs at position, need either number of ( after arithmatic {0}", i);
+                        return false;
+                    }
+                    nonSpaceInput.Add(input[i]);
+                    cur = "arith";
+                }
+                else if (input[i]!=' ')
+                {
+                    nonSpace = "";
+                    Console.WriteLine("error occurs at position {0}, strange letter found", i);
+                    return false;
+                }
+            }
+            nonSpace = nonSpaceInput.ToString();
+            return true;
+        }
         // finds the left symbol, right symbol, left number and right number
 
         private static Dictionary<char, Func<double, double, double>> BasicArithmatic = new Dictionary<char, Func<double, double, double>>()
@@ -35,7 +89,7 @@ namespace ArithCalcV2
             return item1 / item2;
         }
         // this one supports () as first priority then */, similar to a normal calculator
-        public static double Calc(string expression)
+        private static double Calc(string expression)
         {
             // handle the things inside of () first then */ then +-
             // find the deepest level parenth
@@ -43,6 +97,7 @@ namespace ArithCalcV2
             int leftParenthStart = -1;
             int rightParenthStart = -1;
             // find the deepest level parenthethes
+            // if no deepest level parenth left and right are -1
             for (int i = 0; i < expression.Length; i++)
             {
                 if (expression[i] == '(')
@@ -72,6 +127,17 @@ namespace ArithCalcV2
             // there is no parenth
             if (leftParenthStart == -1)
             {
+                // extra right parenth when there are no left parenth present
+                if (expression.Contains(')'))
+                {
+                    Console.WriteLine("left and right parenth don't match, more right parenth than left parenth, exiting program");
+                    System.Environment.Exit(0);
+                }
+                if (expression.Contains('('))
+                {
+                    Console.WriteLine("left and right parenth don't match, more left parenth than left parenth, exiting program");
+                    System.Environment.Exit(0);
+                }
                 return SimpleCalc.Calc(expression);
             }
             // have the deepest level parenth, which means that the inside has no parenthethese
