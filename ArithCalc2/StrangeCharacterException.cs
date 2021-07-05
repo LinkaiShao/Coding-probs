5cf14b95f8fc07c949a97bb79e7dceb316651e40
LinkaiShao <linkai.k.shao@gmail.com>
2021-07-04 22:06:11 -0700

    Add files via upload

---
+++ ArithCalc2/StrangeCharacterException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithCalcV2
+{
+    // there is a character that doesnt belong
+    // need the user original input
+    // need the strange character
+    // need the location of the strange character
+    // cannot inherit from wrongpresentation exception since that is logically not sound
+    class StrangeCharacterException : ArgumentException
+    {
+        public string UserInput { get; }
+        public char StrangeCharacter { get; }
+        public int ErrorLocation { get; }
+
+        public StrangeCharacterException(string message, string userInput, char strangeCharacter, int errorLocation) : base(message)
+        {
+            this.UserInput = userInput;
+            this.ErrorLocation = errorLocation;
+            this.StrangeCharacter = strangeCharacter;
+        }
+    }
+}
---
+++ ArithCalc2/WrongPresentationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithCalcV2
+{
+    // user has acceptable input characters but they are ordered incorrectly, such as 1++2 or (+1)
+    // 1. user's input
+    // 2. base message
+    // 3. where the error occured
+    public class WrongPresentationException : ArgumentException
+    {
+        public string UserInput { get; }
+        public int ErrorLocation { get; }
+        public WrongPresentationException(string message, string userInput, int errorLocation) : base(message)
+        {
+            this.UserInput = userInput;
+            this.ErrorLocation = errorLocation;
+        }
+    }
+}
